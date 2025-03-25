203164d8b963b8c31615b1836d7aa82705ae467c
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-25 16:58:59 +0100

    10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let newArray = [];
-    for (let value of array) {
-      newArray.push(appendString + value);
-    }
-    return newArray;
+  let newArray = [];
+  for (let value of array) {
+    newArray.push(appendString + value);
   }
-  
+  return newArray;
+}
