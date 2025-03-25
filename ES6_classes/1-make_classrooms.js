739461da8dd702de626fec99a4a52de3386a9c91
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-25 22:16:40 +0100

    1-make_classrooms.js

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,10 @@
-import Classroom from 0-classroom.js
-function initializeRooms() {
-  return [
-    ClassRoom(19),
-    ClassRoom(20),
-    ClassRoom(34)
-  ];
+import Classroom from "0-classroom.js"
+import ClassRoom from "./0-classroom";
+export default function initializeRooms() {
+  const size = [19, 20, 34];
+  const ClassArray = [];
+  for (const size of sizes) {
+    ClassArray.push(new ClassRoom(size));
+  }
+  return ClassArray;
 }
