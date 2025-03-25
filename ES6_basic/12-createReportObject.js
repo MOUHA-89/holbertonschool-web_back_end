1c6c593b2959059b1bb94c5628ca7b89fe1e0031
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-25 17:26:39 +0100

    12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments() {
-        return Object.keys(this.allEmployees).length;
-      },
-    };
-  }
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+}
   
