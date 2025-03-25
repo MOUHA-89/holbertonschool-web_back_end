29736008d9a74e9408698c353a267eb103e5f00a
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-25 16:52:43 +0100

    8-getBudgetCurrentYear.js

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,13 +1,13 @@
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
-  }  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const currentYear = getCurrentYear();
-    
-  return {
+}
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const currentYear = getCurrentYear();
+  const budget = {
     [`income-${currentYear}`]: income,
     [`gdp-${currentYear}`]: gdp,
-    [`capita-${currentYear}`]: capita
+    [`capita-${currentYear}`]: capita,
   };
+  return budget;
 }
