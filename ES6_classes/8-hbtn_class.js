abcec3c1e24dd562537b7cca2182522c99fff927
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-26 18:50:08 +0100

    8-hbtn_class.js

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class Holbertonclass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueof() {
+    return this._size;
+  }
+}
