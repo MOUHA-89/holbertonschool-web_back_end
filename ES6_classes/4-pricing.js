5b04834f5532f63e4fd5ea0ee6782622d3caebeb
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-26 12:11:25 +0100

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be an Number');
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('currency must be an Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount_currency_name} (${this._currency_code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
