8bfbf371050671febddbc1d5bf7d6a9ca049718c
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-31 20:49:03 +0200

    0-get_list_students.js

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudenysByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
   }
-  return students.filter(student=>student.location === city);
+  return students.filter((student) => student.location === city);
 }
