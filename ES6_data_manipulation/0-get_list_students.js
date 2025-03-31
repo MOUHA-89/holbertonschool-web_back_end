89e342ad05f71b50a767f2ccf835e572bfbace7b
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-31 18:35:56 +0200

    0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,7 @@
 export default function getListStudents() {
-  const v1 = { id: 1, firstName: 'Guillaume', location: 'San fransico' };
-  const v2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const v3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  const v1 = { id: 1, firstName: 'Guillaume', location: 'San fransico', };
+  const v2 = { id: 2, firstName: 'James', location: 'Columbia', };
+  const v3 = { id: 5, firstName: 'Serena', location: 'San Francisco', };
 
   return [v1, v2, v3];
 }
