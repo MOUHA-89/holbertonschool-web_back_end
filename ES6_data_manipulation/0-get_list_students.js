8765b47e8cae15b700e5988247efda7cab46e963
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-03-31 17:57:21 +0200

    0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,23 @@
 function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
-console.log(getListStudents());
+    return [
+      {
+        id: 1,
+        firstName: "Guillaume",
+        location: "San Francisco",
+      },
+      {
+        id: 2,
+        firstName: "James",
+        location: "Columbia",
+      },
+      {
+        id: 5,
+        firstName: "Serena",
+        location: "San Francisco",
+      },
+    ];
+  }
+  
+  // Example usage:
+  console.log(getListStudents());
+  
