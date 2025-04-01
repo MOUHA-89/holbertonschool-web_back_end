5c0e88c81052013c8bdf03b41cad17bc67a8bfc3
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-01 13:38:40 +0200

    10-update_uniq_items.js

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
-export default function updateUniqueItems(map) {
-    const update = new Map()
-    if (!items instanceof map) {
-        throw new Error('cannot process');
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('cannot process');
+  }
+  for (const [key, value] of items) {
+    if (value === 1) {
+      items.set(key, 100);
     }
-    for (const [key, value] of items) {
-        if (value === 1) {
-            items.set(key, 100);
-        }
-    }
+  }
 
-    return items; 
+  return items;
 }
