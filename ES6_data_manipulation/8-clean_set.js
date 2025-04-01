68a0dc1059bd755c4c53b55fade297290840f7c9
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-01 11:52:08 +0200

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,10 +3,10 @@ export default function cleanSet(set, startString) {
     return '';
   }
   const result = [];
-  
+
   for (const value of set) {
     if (value.startsWith(startString)) {
-    result.push(value.slice(startString.length));
+      result.push(value.slice(startString.length));
     }
   }
   return result.join('-');
