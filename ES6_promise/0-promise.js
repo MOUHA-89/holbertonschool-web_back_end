37a175275c6a08d77245ccaff06510c89b70ed66
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-01 23:35:41 +0200

    0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,12 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
+  
     const success = true;
+
     if (success) {
-      resolve(" Success!");
-    }
-    else {
-      reject("Failled!");
+      resolve("Success!");
+    } else {
+      reject(new Error("Failed!"));
     }
   });
- });
 }
