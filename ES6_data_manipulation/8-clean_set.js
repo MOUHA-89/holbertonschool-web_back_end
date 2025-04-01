3c4984b0ef0c9e2d4f7e3b783fd52704dbd229da
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-01 11:46:20 +0200

    8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,14 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
-    return '';
-  }
-  const result = [];
-
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result.push(value.slice(startString.length));
+    if (!startString) {
+      return '';
     }
+    const result = [];
+  
+    for (const value of set) {
+      if (value.startsWith(startString)) {
+        result.push(value.slice(startString.length));
+      }
+    }
+    return result.join('-');
   }
-  return result.join('-');
-}
+  
