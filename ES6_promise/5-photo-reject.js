2422bed9622058e189419838739d0edced1a5551
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-02 15:14:33 +0200

    5-photo-reject.js

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,9 +1,3 @@
 export default function uploadPhoto(fileName) {
-    return Promise((resolve, reject) => {
-        if(fileName) {
-            resolve('Success!');
-        } else {
-            reject(Error(`$('fileName cannot be processed')`));
-        }
-    });
+  return Promise.reject(Error(`${filename} cannot be processed`));
 }
