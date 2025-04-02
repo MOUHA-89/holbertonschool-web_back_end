851c83bf8b8be17e3c42ca9f98b33040ed705ad8
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-02 11:52:59 +0200

    2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Succcess' })).catch(() => new Error())
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  return promise.then(() => ({ status: 200, body: 'Succcess' })).catch(() => new Error()).finally(() => {
+    console.log('Got a response from the API');
+  });
 }
