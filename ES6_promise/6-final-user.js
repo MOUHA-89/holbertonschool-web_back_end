c336fc917287de9e58447419d5225eb8e679f8a2
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-02 16:21:26 +0200

    6-final-user.js

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+  return Promise.allSettled(promises)
+    .then((results) => {
+      results.map((result) => ({
+        Status: result.Status,
+        Value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+    });
+}
