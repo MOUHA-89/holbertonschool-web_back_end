268670917017990532f820f55f5548a13d60c0f7
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-23 22:56:35 +0200

    8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def make_multiplier(multiplier: float) -> float:
+    """
+    Returns a function that multiplies a float by multiplier.
+    """
+    return float * multiplier
