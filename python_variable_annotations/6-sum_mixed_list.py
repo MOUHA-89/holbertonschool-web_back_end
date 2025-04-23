c7c326552002c997d03824366db65678e0b1aa63
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-23 22:35:40 +0200

    6-sum_mixed_list.py

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,13 +2,13 @@
 """
 Python programm
 """
-from typing import List
+from typing import List, Union
 """
 Define and annotate a function sum!_list that takes a list
 """
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Returns the sum of a mixed list of integers and floats.
     """
