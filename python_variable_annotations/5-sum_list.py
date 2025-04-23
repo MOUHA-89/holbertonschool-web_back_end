8f61c39b628e4885fbbacdad1ec3cbd99744ce65
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-23 22:20:34 +0200

    5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
+from typing import List
 """
 Define and annotate a function sum_list that takes a list
 of floats as an argument
 """
 
 
-def sum_list(input_list: [float]) -> [float]:
+def sum_list(input_list: List[float]) -> float:
     """
     Returns the sum of a list of floats.
     """
