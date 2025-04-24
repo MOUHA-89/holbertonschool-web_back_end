0b444671d44c298dc4b93d18964fb31fa42b50d0
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-24 21:06:52 +0200

    4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -4,15 +4,15 @@ Python programm
 """
 import asyncio
 from typing import List
-from basic_async_syntax import wait_random
+from basic_async_syntax import task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List:
     """
     Function that spawns wait_random n times with the specified max_delay
     """
     delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
+    for _ in range(max_delay):
+        delay = await task_wait_random(max_delay)
         delays.append(delay)
     return delays
