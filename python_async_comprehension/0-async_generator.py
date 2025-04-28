f43fde11d9ec06356fae9f8602f4d8f2bafba064
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-28 10:40:40 +0200

    0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-python programm
+this module contains an asynchronous generator function
 """
 import random
 import asyncio
@@ -12,4 +12,4 @@ async def async_generator():
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.randint(0, 10)
