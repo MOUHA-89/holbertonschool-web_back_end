0e5a892215caddf2f98a0c8e5bf3355258dd63b7
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-29 10:09:35 +0200

    0-simple_helper_function.py

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Returns a tuple of size two containing a start index and an end index"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ Returns a tuple of size two containing
+    a start index and an end index"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
