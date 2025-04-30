1d7b2465a96dd4839cc8353f67a55930ddfc233b
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-04-30 14:38:17 +0200

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python3
-""" Module for using PyMongo to parse nginx logs """
-
 from pymongo import MongoClient
 
+def nginx_stats():
+    client = MongoClient()
+    db = client.logs
+    collection = db.nginx
 
-# default host:port is localhost:27017
-client = MongoClient()
-col = client.logs.nginx
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
 
-# have to use empty {} to get count of all docs!
-count = col.count_documents({})
-get = col.count_documents({"method": "GET"})
-post = col.count_documents({"method": "POST"})
-put = col.count_documents({"method": "PUT"})
-patch = col.count_documents({"method": "PATCH"})
-delete = col.count_documents({"method": "DELETE"})
-status = col.count_documents({"method": "GET", "path": "/status"})
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
 
 if __name__ == "__main__":
-    print(f"{count} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{status} status check")
+    nginx_stats()
