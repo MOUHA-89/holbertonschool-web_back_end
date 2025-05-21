9c36018ca1e86e70bbe599a5ebba78e9dbd55cc2
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-05-21 16:13:27 +0200

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
-process.stdin.setEncoding('utf8');
-console.log('Welcome to Holberton School, what is your name?:');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
-  process.exit();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
-process.on('exit', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
-module.exports = process.stdin.on;
