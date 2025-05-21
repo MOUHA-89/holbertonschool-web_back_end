912b0acaa1eeadb20c2beeb9e42da99813eaed1b
MOUHA-89 <mouhamedkeita05@gmail.com>
2025-05-21 16:16:13 +0200

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-process.stdin.on('end', () => {
+process.on('end', () => {
   console.log('This important software is now closing');
 });
